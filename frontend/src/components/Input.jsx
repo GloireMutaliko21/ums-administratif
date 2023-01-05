2749c6f5ab17c2879c49edbc800b5849337f1be8
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 15:29:20 +0100

    add button and input components

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,47 @@
+import { useStateContext } from '../context/contextProvider'
+
+const Input = ({
+    style,
+    placeholder,
+    disabled, label,
+    value, onChange,
+    type, name,
+    icon, iconMask,
+    reference
+}) => {
+
+    const { boolingStates, setBoolingStates } = useStateContext();
+
+    const handleChangeIcon = () => {
+        setBoolingStates(prevState => {
+            return { ...prevState, showPassword: !prevState.showPassword }
+        });
+    };
+
+    return (
+        <div>
+            <label className='font-bold text-sm'>{label}</label>
+            <div className='bg-gray-200 flex justify-between items-center relative my-2'>
+                <input
+                    ref={reference}
+                    className={`${style} text-gray-700 text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block appearance-none w-full`}
+                    type={type}
+                    value={value}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    name={name}
+                    disabled={disabled}
+                >
+                </input>
+                <span
+                    className='absolute right-3 hover:cursor-pointer text-lg text-teal-900'
+                    onClick={handleChangeIcon}
+                >
+                    {!boolingStates.showPassword ? icon : iconMask}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Input;
