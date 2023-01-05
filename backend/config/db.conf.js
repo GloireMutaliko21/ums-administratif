1a72feee0b3223bf18665ac5e3090ba8a6d4b587
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 08:19:38 +0100

    feat: backend setup database connection

---
+++ backend/config/db.conf.js
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbpassword = process.env.DB_PWD;
+const dbHost = process.env.DB_HOST;
+
+export const dbSequelize = new Sequelize(dbName, dbUser, dbpassword, {
+    dialect: 'mysql',
+    host: dbHost
+});
