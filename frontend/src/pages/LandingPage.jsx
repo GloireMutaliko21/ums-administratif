a2fe526e8562d934f4dfd40ca75d02fa5fe84109
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 18:00:04 +0100

    style: landing page background

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
+import "../../public/styles/landing.css"
+import bg from "../assets/images/university.jpg";
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <section className="bg-black/30">
+            <h3>OK</h3>
+        </section>
     );
 }
 
