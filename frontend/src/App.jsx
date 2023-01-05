109c40bef8f5238ed4cf0a0fec6d04dd35849b0a
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 07:32:50 +0100

    style: setup tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-9xl font-bold'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
