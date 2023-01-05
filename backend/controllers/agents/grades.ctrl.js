fe2c741e019ef936567e94baec751eb2a314a663
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 10:02:00 +0100

    feat: back-add getGrades controller and router

--- backend/controllers/agents/grades.ctrl.js
+++ backend/controllers/agents/grades.ctrl.js
@@ -14,3 +14,18 @@ export const createGrade = async (req, res, next) => {
         return next(error);
     }
 };
+
+export const getGrades = async (req, res, next) => {
+    try {
+        const grades = await Grades.findAll();
+        if (!grades) {
+            res.status(404).json('No grade founded');
+            return;
+        }
+        res.status(200).json({ data: grades });
+    } catch (err) {
+        const error = new Error(err);
+        res.status(500);
+        return next(error);
+    }
+};
--- backend/routes/agents/grades.routes.js
+++ backend/routes/agents/grades.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createGrade } from "../../controllers/agents/grades.ctrl.js";
+import { createGrade, getGrades } from "../../controllers/agents/grades.ctrl.js";
 
 const router = express.Router();
 
 router
+    .get('/grades', getGrades)
+
     .post('/grades/new', createGrade)
 
 export default router;
