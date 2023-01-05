86a71db4e3abe910dd0f875c5b8f7795a6c415af
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 14:55:30 +0100

    feat: back-create authAll middleware

--- backend/middlewares/auth.mid.js
+++ backend/middlewares/auth.mid.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import Agent from '../models/agents/agents.mdl.js';
 
-const authDirection = async (req, res, next) => {
+export const authDirection = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
@@ -13,6 +13,19 @@ const authDirection = async (req, res, next) => {
             next();
         }
         res.status(401).json({ err: 'Access Denied' });
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+};
+
+export const authAll = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+        const agentId = decodedToken.agentId;
+
+        req.user = await Agent.findByPk(agentId);
+        next();
     } catch (err) {
         res.status(401).json({ err });
     }
