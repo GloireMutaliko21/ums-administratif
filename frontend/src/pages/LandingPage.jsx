00fc57ccd440b8106ec407712bde0874912e5059
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-05 18:19:54 +0100

    style: init landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,8 +3,13 @@ import bg from "../assets/images/university.jpg";
 
 const LandingPage = () => {
     return (
-        <section className="bg-black/30">
-            <h3>OK</h3>
+        <section>
+            <div className="content h-full w-full bg-black/70 flex flex-col justify-center items-center">
+                <h3 className="font-black text-9xl text-white">UMS</h3>
+                <div className="text-slate-300">
+                    <p>University Managment System</p>
+                </div>
+            </div>
         </section>
     );
 }
