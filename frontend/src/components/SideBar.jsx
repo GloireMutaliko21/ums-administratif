06ffa9d21b194fb3eb3bfdef374722b616b488bd
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-06 14:07:15 +0100

    init client sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,15 +1,16 @@
 
-import { NavLink } from 'react-router-dom';
-import { FiUsers, FiTarget } from 'react-icons/fi';
-import { MdAttachMoney, MdSocialDistance, MdOutlineAccountBalance } from 'react-icons/md';
-import { SiProcesswire } from 'react-icons/si';
-import { VscTasklist } from 'react-icons/vsc';
-import { AiFillWechat } from 'react-icons/ai';
 import SideBarAdmin from '../admin/components/SideBar';
+import SideBarClient from '../client/components/SideBar';
 const SideBar = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
     return (
         <nav className="w-56 fixed top-10 bottom-0 py-5 px-3 bg-black text-pink-200 h-screen shadow-2xl shadow-pink-200">
-            <SideBarAdmin />
+            {
+                user.agent.privilege === 'direction' ?
+                    <SideBarAdmin /> :
+                    <SideBarClient />
+            }
         </nav>
     );
 }
