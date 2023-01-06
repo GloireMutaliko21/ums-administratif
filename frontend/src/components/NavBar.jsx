8c4b4bdd88b3055a39ef5fe7bb88a1fdbae77dca
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-06 11:34:06 +0100

    style: init side and nav bar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <nav className='bg-white flex justify-between items-center fixed top-0 right-0 shadow-lg 2xl:shadow p-3 w-full left-0 2xl:w-[1290px] 2xl:left-auto 2xl:right-auto z-50'>
+            NAVBAR
+        </nav>
+    );
+}
+
+export default NavBar;
---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+
+const SideBar = () => {
+    return (
+        <div className="w-56 fixed top-10 bottom-0 py-5 px-3">
+            SideBar
+        </div>
+    );
+}
+
+export default SideBar;
