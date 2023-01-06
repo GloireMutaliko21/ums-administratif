057145f52573deab850e2f22798c84af3426a497
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-06 14:22:34 +0100

    feat: front-init direction routes paths

--- frontend/src/admin/routes/mainRoutes.routes.jsx
+++ frontend/src/admin/routes/mainRoutes.routes.jsx
@@ -1,5 +1,47 @@
+import Agents from '../screens/Agents';
+import PresencesConges from '../screens/PresencesConges';
+import Paie from '../screens/Paie';
+import Boarding from '../screens/Boarding';
+import CasSoc from '../screens/CasSoc';
+import TaskList from '../screens/TaskList';
+import Patrimoine from '../screens/Patrimoine';
+import Chat from '../screens/Chat';
+
 export const mainRoutes = [
     {
-        path: '/'
+        path: '/',
+        element: <Agents />
+    },
+    {
+        path: '/agents',
+        element: <Agents />
+    },
+    {
+        path: '/pres-conges',
+        element: <PresencesConges />
+    },
+    {
+        path: '/paie',
+        element: <Paie />
+    },
+    {
+        path: '/on-offboarding',
+        element: <Boarding />
+    },
+    {
+        path: '/cassoc',
+        element: <CasSoc />
+    },
+    {
+        path: '/taskList',
+        element: <TaskList />
+    },
+    {
+        path: '/patrimoine',
+        element: <Patrimoine />
+    },
+    {
+        path: '/chat',
+        element: <Chat />
     },
 ];
