c2d987c35d91fe00e7e6aacb37d0e7150eb00ef0
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-06 15:56:22 +0100

    front-init agentItem list

--- frontend/src/admin/components/AgentItem.jsx
+++ frontend/src/admin/components/AgentItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const AgentItem = () => {
+const AgentItem = ({ matricule, nom, postnom, prenom, statut, titre }) => {
     return (
         <article className='flex justify-around w-full'>
-
+            <p>{matricule}</p>
+            <p>{nom}</p>
+            <p>{postnom}</p>
+            <p>{prenom}</p>
+            <p>{statut}</p>
+            <p>{titre}</p>
         </article>
     )
 }
