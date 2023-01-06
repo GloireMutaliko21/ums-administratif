20b9914b106ff4deaf0d4434748e6ec4634affe1
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-06 14:17:53 +0100

    front-init direction routes

---
+++ frontend/src/admin/screens/Paie.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Paie = () => {
+    return (
+        <div>Paie</div>
+    )
+}
+
+export default Paie
---
+++ frontend/src/admin/screens/PresencesConges.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PresencesConges = () => {
+    return (
+        <div>PresencesConges</div>
+    )
+}
+
+export default PresencesConges
