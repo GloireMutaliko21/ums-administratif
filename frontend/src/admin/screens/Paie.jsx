51720eb4a045a9c79680b9ef3fd5549ce5116dc9
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-10 17:59:35 +0100

    style: paie navbar

--- frontend/src/admin/screens/Paie.jsx
+++ frontend/src/admin/screens/Paie.jsx
@@ -6,13 +6,13 @@ import { paiemLinks } from '../data/componentsData';
 const Paie = () => {
     return (
         <div className='relative'>
-            <section className='flex justify-between text-slate-500 bg-white p-2 border-b shadow-sm w-full'>
+            <section className='flex justify-between text-slate-500 text-sm bg-white p-2 border-b shadow-sm w-full'>
                 {
                     paiemLinks.map(({ path, titre }) =>
                         <NavLink
                             key={path}
                             to={`/index/paie/${path}`}
-                            className={({ isActive }) => `${isActive && 'text-sky-500 border-b-2'} mx-3`}
+                            className={({ isActive }) => `${isActive && 'text-sky-500 rounded-xl border border-sky-500'} mx-3 hover:text-sky-500 hover:border hover:border-sky-500 hover:rounded-xl px-2 duration-300`}
                         >
                             {titre}
                         </NavLink>
