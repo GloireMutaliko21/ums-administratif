99f31ea47f241cd56d9a8f49424d0b2d412e9ada
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-11 08:25:19 +0100

    feat: back-create model maladieConge

---
+++ backend/models/paie/maladConge.mdl.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+
+import { dbSequelize } from "../../config/db.conf.js";
+
+const MaladConge = dbSequelize.define('maladConge', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    jours: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    taux: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    libelle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+export default RemunConge;
