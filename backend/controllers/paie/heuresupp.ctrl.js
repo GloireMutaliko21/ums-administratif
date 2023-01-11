7a960a00c07ca3065101cf4458dea79fa29eae82
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-11 11:45:09 +0100

    feat: back-return total heure supp per agent

--- backend/controllers/paie/heuresupp.ctrl.js
+++ backend/controllers/paie/heuresupp.ctrl.js
@@ -1,3 +1,6 @@
+import { Op, Sequelize, QueryTypes } from 'sequelize';
+
+import { dbSequelize } from "../../config/db.conf.js";
 import HeureSupp from "../../models/paie/heuresupp.mdl.js";
 
 export const registerHeureSupp = async (req, res, next) => {
@@ -9,6 +12,32 @@ export const registerHeureSupp = async (req, res, next) => {
         //Find This created heuresupp remuneration and send it to front
         const createdHeureSupp = await HeureSupp.findByPk(heureSupp.id, { include: 'agent' });
         res.status(201).json({ data: createdHeureSupp });
+    } catch (err) {
+        const error = new Error(err);
+        res.status(500);
+        return next(error);
+    }
+};
+
+export const getHeureSuppPerAgent = async (req, res, next) => {
+    try {
+        const { agentId } = req.params;
+        const { mounth } = req.query;
+
+        // const heuresSupp = await HeureSupp.findAll({
+        //     attributes: [[dbSequelize.fn('SUM', dbSequelize.col('nombre') * dbSequelize.col('taux')), 'Total']],
+        //     where: {
+        //         [Op.and]: [
+        //             { agentId },
+        //             { createdAt: { [Op.startsWith]: mounth } }
+        //         ]
+        //     }
+        // });
+
+        const heuresSupp = await dbSequelize.query(`SELECT sum(nombre * taux) AS sum FROM heureSupps WHERE (agentId = '${agentId}' AND createdAt LIKE '${mounth}%')`, { type: QueryTypes.SELECT });
+
+        res.status(200).json({ data: heuresSupp });
+
     } catch (err) {
         const error = new Error(err);
         res.status(500);
--- backend/routes/paie/heuresupp.routes.js
+++ backend/routes/paie/heuresupp.routes.js
@@ -5,6 +5,8 @@ import * as heuresSuppCtrl from '../../controllers/paie/heuresupp.ctrl.js';
 const router = express.Router();
 
 router
+    .get('/heuresupp/:agentId', heuresSuppCtrl.getHeureSuppPerAgent)
+
     .post('/heuresupp/new', heuresSuppCtrl.registerHeureSupp);
 
 export default router;
