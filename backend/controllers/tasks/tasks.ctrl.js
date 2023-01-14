204f59128d65b43c0efb72bbfe5e2926d4315614
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-14 14:53:00 +0100

    back-init task controllers

---
+++ backend/controllers/tasks/tasks.ctrl.js
@@ -0,0 +1,26 @@
+import { dbSequelize } from "../../config/db.conf.js";
+import Task from "../../models/tasks/task.mdl.js";
+
+export const createTask = async (req, res, next) => {
+
+};
+
+export const getTasks = async (req, res, next) => {
+
+};
+
+export const getTasksDay = async (req, res, next) => {
+
+};
+
+export const getTask = async (req, res, next) => {
+
+};
+
+export const updateTask = async (req, res, next) => {
+
+};
+
+export const deleteTask = async (req, res, next) => {
+
+};
