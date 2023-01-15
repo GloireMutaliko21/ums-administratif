2a6d26b9a2dd7bf0105be30dc09e106f3afa2ea7
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-15 11:01:51 +0100

    feat:front-limit routes access

--- frontend/src/client/routes/mainRoutes.routes.jsx
+++ frontend/src/client/routes/mainRoutes.routes.jsx
@@ -2,10 +2,9 @@ import PresencesConges from '../screens/PresencesConges';
 import Boarding from '../screens/Boarding';
 import CasSoc from '../screens/CasSoc';
 import TaskList from '../screens/TaskList';
-import Patrimoine from '../screens/Patrimoine';
 import Chat from '../screens/Chat';
 
-export const mainRoutesDirection = [
+export const mainRoutesClient = [
     {
         path: '/index',
         element: <TaskList />
