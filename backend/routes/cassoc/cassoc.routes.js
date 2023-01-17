381dd21dd0500a6240a28145f2f38806b86cfd0f
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-17 22:28:14 +0100

    refact: back-init auth cassoc routes

--- backend/routes/cassoc/cassoc.routes.js
+++ backend/routes/cassoc/cassoc.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import * as cassocCtrl from '../../controllers/cassoc/cassoc.ctrl.js'
+import * as cassocCtrl from '../../controllers/cassoc/cassoc.ctrl.js';
+import * as auth from "../../middlewares/auth.mid.js";
 
 const router = express.Router();
 
 router
-    .post('/new', cassocCtrl.createCasSoc);
+    .post('/new', auth.authAll, cassocCtrl.createCasSoc);
 
 export default router;
