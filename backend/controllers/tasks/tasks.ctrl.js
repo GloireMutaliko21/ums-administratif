377425663347317072cf4df1a5498bf626a780ca
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-17 15:48:20 +0100

    feat: back-return details tasks

--- backend/controllers/tasks/tasks.ctrl.js
+++ backend/controllers/tasks/tasks.ctrl.js
@@ -75,7 +75,9 @@ export const getTasksDay = async (req, res, next) => {
             group: 'status',
             order: ['status']
         });
-        res.status(200).json({ data: tasks });
+        const listTasks = await dbSequelize.query(`SELECT status, JSON_ARRAYAGG(JSON_OBJECT('titre',titre, 'description', description, 'priorite', priorite)) AS data FROM tasks WHERE createdAt LIKE '${NOW.toISOString().slice(0, 10)}%' AND agentId = '${agentId}' GROUP BY status ORDER BY status ASC`, { type: QueryTypes.SELECT })
+
+        res.status(200).json({ data: tasks, list: listTasks });
     } catch (err) {
         console.log(err);
         const error = new Error(err);
