cbfdce1d1c89fd7e388bef93b146cb8d09b925bf
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-17 22:26:40 +0100

    refact: back-init auth grades routes

--- backend/routes/agents/grades.routes.js
+++ backend/routes/agents/grades.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
+
 import { createGrade, getGrades } from "../../controllers/agents/grades.ctrl.js";
+import * as auth from "../../middlewares/auth.mid.js";
 
 const router = express.Router();
 
 router
-    .get('/grades', getGrades)
+    .get('/grades', auth.authDirection, getGrades)
 
-    .post('/grades/new', createGrade);
+    .post('/grades/new', auth.authDirection, createGrade);
 
 export default router;
