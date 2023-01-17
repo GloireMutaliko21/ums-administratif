e44dbef5c1b4905373ffda775d0faf0a9c788a88
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-17 21:53:54 +0100

    feat: back-register cassoc with agetId

--- backend/controllers/cassoc/cassoc.ctrl.js
+++ backend/controllers/cassoc/cassoc.ctrl.js
@@ -5,9 +5,9 @@ import Cassoc from '../../models/cassoc/cassoc.mdl.js';
 
 export const createCasSoc = async (req, res, next) => {
     try {
-        const { description, datefin } = req.body;
+        const { description, datefin, agentId } = req.body;
 
-        const cassoc = await Cassoc.create({ description, datefin });
+        const cassoc = await Cassoc.create({ description, datefin, agentId });
         const createdCas = await Cassoc.findByPk(cassoc.id, { include: 'agent' });
         res.status(201).json({ data: createdCas });
     } catch (err) {
