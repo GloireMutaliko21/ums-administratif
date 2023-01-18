68b2151cd46c4e4d119831b7f6f85b3e400ec0d2
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-18 10:35:55 +0100

    style: display commands cassoc admin

--- frontend/src/admin/screens/CasSoc.jsx
+++ frontend/src/admin/screens/CasSoc.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { handleGet } from '../../api/get';
 import CassocItem from '../../client/components/cassoc/CassocItem';
+import Button from '../../components/Button';
 import CassocHeader from '../../components/CassocHeader';
 import { useStateContext } from '../../context/ContextProvider';
 import { CASSOC_BASE_URL } from '../../utils/constants';
 
 const CasSoc = () => {
     const { cassocFetch, setCassocFetch, cassocList, setCassocList, localUserData } = useStateContext();
+    const [showCommands, setShowCommands] = useState(false);
+    const [selected, setSelected] = useState();
 
     useEffect(() => {
         if (cassocFetch) {
@@ -18,6 +21,25 @@ const CasSoc = () => {
         }
     }, [cassocList]);
 
+    const Commandes = () => {
+        return (
+            <div className="text-xs flex flex-col items-center">
+                <Button
+                    label='Modifier'
+                    style='text-green-600 font-semibold hover:underline hover:text-green-500'
+                />
+                <Button
+                    label='Souscrire'
+                    style='text-teal-600 font-bold hover:underline hover:text-teal-500'
+                />
+                <Button
+                    label='Publier'
+                    style='text-amber-600 font-bold hover:underline hover:text-amber-500'
+                />
+            </div>
+        )
+    };
+
     return (
         <div className='relative'>
             <div>
@@ -26,6 +48,11 @@ const CasSoc = () => {
             <div>
                 <CassocItem
                     data={cassocList?.data}
+                    showCommands={showCommands}
+                    setShowCommands={setShowCommands}
+                    selected={selected}
+                    setSelected={setSelected}
+                    children={<Commandes />}
                 />
             </div>
         </div>
