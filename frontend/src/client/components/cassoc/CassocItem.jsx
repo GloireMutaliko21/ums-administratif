542e90aa269fa6384832e0f9a0f9d2d27a56803d
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-18 09:36:45 +0100

    front-pass props to cassoc item

--- frontend/src/client/components/cassoc/CassocItem.jsx
+++ frontend/src/client/components/cassoc/CassocItem.jsx
@@ -32,52 +32,52 @@ const CassocItem = ({ data }) => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <div className="flex">
-                                            <div className="flex-shrink-0 w-10 h-10">
-                                                <img
-                                                    className="w-full h-full rounded-full"
-                                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                                    alt=""
-                                                />
+                                {data?.map(({ description, datefin, updatedAt, status, agent, id }) =>
+                                    <tr key={id}>
+                                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                            <div className="flex">
+                                                <div className="flex-shrink-0 w-10 h-10">
+                                                    <img
+                                                        className="w-full h-full rounded-full"
+                                                        src={agent.imageUrl}
+                                                        alt={agent.nom}
+                                                    />
+                                                </div>
+                                                <div className="ml-3">
+                                                    <p className="text-gray-900 whitespace-nowrap">
+                                                        {agent.nom} {agent.postnom} {agent.prenom}
+                                                    </p>
+                                                    <p className="text-gray-600 whitespace-nowrap">{agent.matricule}</p>
+                                                </div>
                                             </div>
-                                            <div className="ml-3">
-                                                <p className="text-gray-900 whitespace-nowrap">
-                                                    Molly Sanders
-                                                </p>
-                                                <p className="text-gray-600 whitespace-nowrap">000004</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-nowrap">$20,000</p>
-                                        <p className="text-gray-600 whitespace-nowrap">USD</p>
-                                    </td>
-                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-nowrap">Sept 28, 2019</p>
-                                        <p className="text-gray-600 whitespace-nowrap">Due in 3 days</p>
-                                    </td>
-                                    <td
-                                        className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right"
-                                    >
-                                        <button
-                                            type="button"
-                                            className="inline-block text-gray-500 hover:text-gray-700"
+                                        </td>
+                                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                            <p className="text-gray-900 whitespace-nowrap">{description}</p>
+                                        </td>
+                                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                            <p className="text-gray-900 whitespace-nowrap">{datefin}</p>
+                                            <p className="text-gray-600 whitespace-nowrap">Mis à jour : {updatedAt.toISOString().slice(0, 10)}</p>
+                                        </td>
+                                        <td
+                                            className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right"
                                         >
-                                            <svg
-                                                className="inline-block h-6 w-6 fill-current"
-                                                viewBox="0 0 24 24"
+                                            <button
+                                                type="button"
+                                                className="inline-block text-gray-500 hover:text-gray-700"
                                             >
-                                                <path
-                                                    d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </td>
-                                </tr>
-
-
+                                                <svg
+                                                    className="inline-block h-6 w-6 fill-current"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path
+                                                        d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                                }
                             </tbody>
                         </table>
                     </div>
