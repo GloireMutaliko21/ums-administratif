483670ce9fbe0a50bef6ebd1619f809d410b0b64
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-19 23:24:47 +0100

    feat: back-synthese month data add month require

--- backend/controllers/inventaire/operation.ctrl.js
+++ backend/controllers/inventaire/operation.ctrl.js
@@ -94,7 +94,7 @@ export const ficheStockMonth = async (req, res, next) => {
 export const synteheseMonth = async (req, res, next) => {
     try {
         // const ficheStock = await dbSequelize.query(`SELECT designation, typeOp, SUM(operations.quantite) AS total FROM articles INNER JOIN operations ON articles.id = operations.articleId WHERE month(operations.createdAt) = month(now()) AND year(operations.createdAt) = year(now()) AND articles.id = operations.articleId GROUP BY designation, typeOp ORDER BY designation ASC`, { type: QueryTypes.SELECT })
-        const ficheStock = await dbSequelize.query(`SELECT a.designation, SUM(CASE WHEN o.typeOp = 'entree' THEN o.quantite ELSE 0 END) as entree, SUM(CASE WHEN o.typeOp = 'sortie' THEN o.quantite ELSE 0 END) as sortie FROM articles a LEFT JOIN operations o ON a.id = o.articleId GROUP BY a.designation`, { type: QueryTypes.SELECT })
+        const ficheStock = await dbSequelize.query(`SELECT a.designation, SUM(CASE WHEN o.typeOp = 'entree' THEN o.quantite ELSE 0 END) as entree, SUM(CASE WHEN o.typeOp = 'sortie' THEN o.quantite ELSE 0 END) as sortie FROM articles a LEFT JOIN operations o ON a.id = o.articleId WHERE month(o.createdAt) = month(now()) AND year(o.createdAt) = year(now()) GROUP BY a.designation`, { type: QueryTypes.SELECT })
         if (!ficheStock) {
             res.status(404).json({ data: 'Aucune operation trouvee' });
             return;
@@ -107,22 +107,3 @@ export const synteheseMonth = async (req, res, next) => {
         return next(error);
     }
 };
-
-const data = [
-    {
-        designation: 'Papier',
-        data: {
-            entree: 10,
-            sortie: 20
-        },
-
-    },
-    {
-        designation: 'Papier',
-        data: {
-            entree: 10,
-            sortie: 20
-        },
-
-    },
-]
