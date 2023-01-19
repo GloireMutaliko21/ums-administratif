835ca5f363827246adf998af482da29b4c3e9790
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-19 23:22:09 +0100

    feat: back-init synthese mound data

--- backend/controllers/inventaire/operation.ctrl.js
+++ backend/controllers/inventaire/operation.ctrl.js
@@ -71,7 +71,6 @@ export const ficheStockWeek = async (req, res, next) => {
         }
         res.status(200).json({ data: ficheStock });
     } catch (err) {
-        console.log(err);
         const error = new Error(err);
         res.status(500);
         return next(error);
@@ -86,9 +85,44 @@ export const ficheStockMonth = async (req, res, next) => {
         }
         res.status(200).json({ data: ficheStock });
     } catch (err) {
-        console.log(err);
         const error = new Error(err);
         res.status(500);
         return next(error);
     }
 };
+
+export const synteheseMonth = async (req, res, next) => {
+    try {
+        // const ficheStock = await dbSequelize.query(`SELECT designation, typeOp, SUM(operations.quantite) AS total FROM articles INNER JOIN operations ON articles.id = operations.articleId WHERE month(operations.createdAt) = month(now()) AND year(operations.createdAt) = year(now()) AND articles.id = operations.articleId GROUP BY designation, typeOp ORDER BY designation ASC`, { type: QueryTypes.SELECT })
+        const ficheStock = await dbSequelize.query(`SELECT a.designation, SUM(CASE WHEN o.typeOp = 'entree' THEN o.quantite ELSE 0 END) as entree, SUM(CASE WHEN o.typeOp = 'sortie' THEN o.quantite ELSE 0 END) as sortie FROM articles a LEFT JOIN operations o ON a.id = o.articleId GROUP BY a.designation`, { type: QueryTypes.SELECT })
+        if (!ficheStock) {
+            res.status(404).json({ data: 'Aucune operation trouvee' });
+            return;
+        }
+        res.status(200).json({ data: ficheStock });
+    } catch (err) {
+        console.log(err)
+        const error = new Error(err);
+        res.status(500);
+        return next(error);
+    }
+};
+
+const data = [
+    {
+        designation: 'Papier',
+        data: {
+            entree: 10,
+            sortie: 20
+        },
+
+    },
+    {
+        designation: 'Papier',
+        data: {
+            entree: 10,
+            sortie: 20
+        },
+
+    },
+]
