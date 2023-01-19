9a15aa5c130a4678add8adfb702322c8608dbf67
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-19 13:55:35 +0100

    feat: back-register an unity

--- backend/routes/iventaire/inventaire.routes.js
+++ backend/routes/iventaire/inventaire.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import * as categCtrl from "../../controllers/inventaire/categArticle.ctrl.js";
+import * as uniteCtrl from "../../controllers/inventaire/unities.ctrl.js";
 import { authInventaire } from "../../middlewares/auth.mid.js";
 
 const router = express.Router();
 
 router
     .get('/categ/all', categCtrl.getCategories)
-    .post('/categ/new', authInventaire, categCtrl.createCateg);
+    .post('/categ/new', authInventaire, categCtrl.createCateg)
+    .post('/unite/new', authInventaire, uniteCtrl.createUnity);
 
 export default router;
