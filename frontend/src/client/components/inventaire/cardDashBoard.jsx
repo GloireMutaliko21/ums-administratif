2cdef8536ff91174c0f0504c93dfa302715cf532
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-20 10:41:40 +0100

    feat: front-articles stock inferieur stock alerte

--- frontend/src/client/components/inventaire/cardDashBoard.jsx
+++ frontend/src/client/components/inventaire/cardDashBoard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const cardDashBoard = () => {
+const CardDashBoard = ({ libelle, donne }) => {
     return (
-        <div>
-
+        <div className='p-5 flex justify-between items-center gap-5 shadow border-t border-sky-500'>
+            <h1 className='font-bold text-lg text-slate-500'>{libelle}</h1>
+            <div className='rounded-xl font-black text-2xl border-4 border-sky-200 text-sky-500 h-14 w-14 flex items-center justify-center'>
+                <p>{donne}</p>
+            </div>
         </div>
     );
 }
 
-export default cardDashBoard;
+export default CardDashBoard;
---
+++ frontend/src/client/components/inventaire/cards.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+import { handleGet } from '../../../api/get';
+import { useStateContext } from '../../../context/ContextProvider';
+import { INVENTAIRE_BASE_URL } from '../../../utils/constants';
+import CardDashBoard from './cardDashBoard';
+
+const Cards = () => {
+    const { localUserData } = useStateContext();
+    const [isFetch, setIsFetch] = useState(true);
+    const [unStocked, setUnStocked] = useState();
+
+    useEffect(() => {
+        if (isFetch) {
+            handleGet(localUserData.token, `${INVENTAIRE_BASE_URL}/article/unavailable`, setUnStocked, null);
+        }
+        return () => {
+            setIsFetch(false);
+        }
+    }, [unStocked]);
+
+
+    return (
+        <div className='flex justify-around w-full gap-4'>
+            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
+            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
+            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
+            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
+        </div>
+    );
+}
+
+export default Cards;
