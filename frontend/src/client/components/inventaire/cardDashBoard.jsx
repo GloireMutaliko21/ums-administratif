d3467e84ff9cb04aefc271fdb1d0826946780118
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-20 14:07:45 +0100

    style: etiquettes entrees sorties

--- frontend/src/client/components/inventaire/cardDashBoard.jsx
+++ frontend/src/client/components/inventaire/cardDashBoard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
-const CardDashBoard = ({ libelle, donne, path, link }) => {
+const CardDashBoard = ({ libelle, donne, path, link, color, borderColor }) => {
     return (
         <div className='p-5 shadow border-t border-sky-500'>
             <div className='flex flex-col justify-between items-center gap-5'>
                 <h1 className='font-bold text-lg text-slate-500'>{libelle}</h1>
                 <div className='flex items-end justify-between w-full'>
-                    <p className='rounded-xl font-black text-2xl border shadow-md border-slate-300 text-teal-700 h-14 w-14 flex items-center justify-center'>{donne}</p>
+                    <p className={`rounded-xl font-black text-2xl border shadow-md ${borderColor} text-${color} h-14 w-14 flex items-center justify-center`}>{donne}</p>
                     <NavLink to={path} className='text-sm text-sky-500 hover:underline pb-2'>
                         {link}
                     </NavLink>
--- frontend/src/client/components/inventaire/cards.jsx
+++ frontend/src/client/components/inventaire/cards.jsx
@@ -10,24 +10,26 @@ const Cards = () => {
     const [isFetch, setIsFetch] = useState(true);
     const [unStocked, setUnStocked] = useState();
     const [totArticles, setTotArticles] = useState();
+    const [ficheToday, setFicheToday] = useState();
 
     useEffect(() => {
         if (isFetch) {
             handleGet(localUserData.token, `${INVENTAIRE_BASE_URL}/article/unavailable`, setUnStocked, null);
             handleGet(localUserData.token, `${INVENTAIRE_BASE_URL}/article/all`, setTotArticles, null);
+            handleGet(localUserData.token, `${INVENTAIRE_BASE_URL}/operation/fiche/today`, setFicheToday, null);
         }
         return () => {
             setIsFetch(false);
         }
-    }, [unStocked, totArticles]);
+    }, [unStocked, totArticles, ficheToday]);
 
 
     return (
         <div className='flex flex-wrap justify-around w-full gap-4'>
-            <CardDashBoard donne={unStocked?.data?.length > 0 ? unStocked?.data?.length : 0} libelle='Articles en alerte' link='Détails' />
-            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
-            <CardDashBoard donne={unStocked?.data?.length} libelle='Articles en alerte' />
-            <CardDashBoard donne={totArticles?.data?.length > 0 ? totArticles?.data?.length : 0} libelle='Total articles' />
+            <CardDashBoard donne={unStocked?.data?.length > 0 ? unStocked?.data?.length : 0} libelle='Articles en alerte' link='Détails' color='red-600' borderColor='border-red-300' />
+            <CardDashBoard donne={ficheToday?.data?.length > 0 ? ficheToday?.data?.length > 1 ? ficheToday?.data[0]?.data?.length : ficheToday?.data[0]?.typeOp === 'entree' ? ficheToday?.data[0]?.data?.length : 0 : 0} libelle='Entrées journalières' color='green-600' borderColor='border-green-300' link='Fiche' />
+            <CardDashBoard donne={ficheToday?.data?.length > 0 ? ficheToday?.data?.length > 1 ? ficheToday?.data[1]?.data?.length : ficheToday?.data[0]?.typeOp === 'sortie' ? ficheToday?.data[1]?.data?.length : 0 : 0} libelle='Sorties journalières' color='blue-600' borderColor='border-blue-300' link='Fiche' />
+            <CardDashBoard donne={totArticles?.data?.length > 0 ? totArticles?.data?.length : 0} libelle='Total articles' link='Tout' color='teal-600' borderColor='border-teal-300' />
         </div>
     );
 }
