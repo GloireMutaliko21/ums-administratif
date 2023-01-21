92b18472f2c6cae6d2a87690899fc6aba96bc23c
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-21 12:20:16 +0100

    feat: add articles grid

--- frontend/src/client/screens/inventaire/Articles.jsx
+++ frontend/src/client/screens/inventaire/Articles.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Sort, Page, Search, Toolbar, Inject } from "@syncfusion/ej2-react-grids";
 
 import { handleGet } from '../../../api/get';
 import { useStateContext } from '../../../context/ContextProvider';
@@ -18,10 +19,54 @@ const Articles = () => {
 
     console.log(articlesList);
 
+    const articlesGrid = [
+        {
+            headerText: 'Designation',
+            // width: '150',
+            field: 'designation',
+            textAlign: 'Center'
+        },
+        {
+            headerText: 'Stock',
+            field: 'quantite',
+            // width: '90',
+            textAlign: 'Center',
+        },
+        {
+            headerText: 'Unité',
+            // width: '120',
+            textAlign: 'Center',
+            field: 'unite.libelle'
+        },
+
+        {
+            headerText: 'Catégorie',
+            field: 'categArtcle.libelle',
+            // width: '135',
+            textAlign: 'Center'
+        },
+    ];
+
     return (
         <div className='p-2'>
             <div>
                 <h1 className='font-bold text-3xl text-slate-700 text-center p-1 border-b bg-gray-100'>Liste des articles</h1>
+            </div>
+            <div className='m-2 bg-white rounded-3xl'>
+                <GridComponent
+                    dataSource={articlesList?.data}
+                    allowPaging
+                    allowSorting
+                    toolbar={['Search']}
+                    width='auto'
+                >
+                    <ColumnsDirective>
+                        {articlesGrid.map((item, index) => (
+                            <ColumnDirective key={index} {...item} />
+                        ))}
+                    </ColumnsDirective>
+                    <Inject services={[Sort, Page, Search, Toolbar]} />
+                </GridComponent>
             </div>
         </div>
     );
