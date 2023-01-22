370062c1acfebde9e59691d6bf915db6ef42a0f2
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-22 07:10:18 +0100

    feat: front-register unities

--- frontend/src/client/screens/inventaire/Configurations.jsx
+++ frontend/src/client/screens/inventaire/Configurations.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddCategorie from './AddCategorie';
+import AddUnite from './AddUnite';
 
 const Configurations = () => {
     return (
         <div className='grid grid-cols-3'>
             <AddCategorie />
+            <AddUnite />
         </div>
     );
 }
