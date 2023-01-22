789f46f50710a6cd65e367ff2edcf9a8f9787bf7
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-22 11:47:54 +0100

    feat: fiches on inventaires

--- frontend/src/client/screens/inventaire/Configurations.jsx
+++ frontend/src/client/screens/inventaire/Configurations.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddCategorie from './AddCategorie';
 import AddUnite from './AddUnite';
+import Fiches from './Fiches';
 
 const Configurations = () => {
     return (
         <div className='grid grid-cols-3'>
+            <Fiches />
             <AddCategorie />
             <AddUnite />
         </div>
