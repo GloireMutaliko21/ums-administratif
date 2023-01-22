5fd483737d601d2a6473fed6f91822a7f4bf0fca
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-22 08:33:19 +0100

    feat: front-invent dashboard admin

---
+++ frontend/src/admin/components/patrimoine/InventaireAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardInvent from '../../../client/screens/inventaire/Dashboard';
+
+const InventaireAdmin = () => {
+    return (
+        <div className='mt-5'>
+            <DashboardInvent />
+        </div>
+    );
+}
+
+export default InventaireAdmin;
