ba79260f179f6a2c160f696ed0e1279d00f5814e
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-22 12:55:56 +0100

    feat: back-add a categBien model

---
+++ backend/models/patrimoine/categBien.mdl.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+
+import { dbSequelize } from "../../config/db.conf.js";
+
+const CategBien = dbSequelize.define('categBien', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    libelle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dureeVie: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+export default CategBien;
