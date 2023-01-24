85e025e9bcfef77c44a4ab21701ad42d6d13636d
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-24 16:07:03 +0100

    style: menu profile

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { FaUniversity } from "react-icons/fa";
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { NavLink, useNavigate } from "react-router-dom";
+import { SiDatabricks } from "react-icons/si";
+import { TfiFolder } from "react-icons/tfi";
+import { VscAccount } from "react-icons/vsc";
+import { RiArrowDropDownLine, RiCloseCircleLine, } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 import { useStateContext } from "../context/ContextProvider";
 import Button from './Button';
+import useTransition from '../hook/useTransition';
 
 
 const NavBar = () => {
-    const { localUserData, loginStatus, setLoginStatus } = useStateContext();
+    const { localUserData, setLoginStatus } = useStateContext();
+
+    const [showMenu, setShowMenu] = useState(false);
+    const hasTransitionedIn = useTransition(showMenu, 500);
+
+
+    const handleChangeShowProfileMenu = () => setShowMenu(state => !state);
 
     const navigate = useNavigate();
 
@@ -18,6 +29,36 @@ const NavBar = () => {
         navigate('/');
     }
 
+    function ProfileMenu() {
+        return (
+            <div>
+                <div className="flex items-center justify-between border-b pb-2 mb-3">
+                    <h1 className="font-bold text-2xl text-slate-700 text-left">Profile</h1>
+                    <div
+                        className="text-2xl text-amber-600 cursor-pointer shadow-md rounded-full border-t hover:shadow-xl hover:bg-slate-50 p-2"
+                        onClick={() => setShowMenu(false)}
+                    >
+                        <RiCloseCircleLine />
+                    </div>
+                </div>
+                <div className="text-left text-slate-600 text-base">
+                    <div className="flex items-center gap-5 mb-4">
+                        <SiDatabricks className="text-amber-500" />
+                        <p>Données personnelles</p>
+                    </div>
+                    <div className="flex items-center gap-5 mb-4">
+                        <TfiFolder className="text-amber-500" />
+                        <p>Dossier</p>
+                    </div>
+                    <div className="flex items-center gap-5 mb-4">
+                        <VscAccount className="text-amber-500" />
+                        <p>Compte</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <nav className='bg-white flex justify-between items-center fixed top-0 right-0 shadow-sm 2xl:shadow p-2 w-full left-0 z-50'>
             <div className="flex items-center gap-2">
@@ -25,7 +66,9 @@ const NavBar = () => {
                 <p className="font-bold text-amber-600 text-sm">University Managment System </p>
             </div>
             <div className="flex items-center gap-7 mr-4">
-                <div className="flex items-center gap-1 text-sm text-slate-600 border border-sky-200 rounded px-5 py-px cursor-pointer">
+                <div className="flex items-center gap-1 text-sm text-slate-600 border border-sky-200 rounded px-5 py-px cursor-pointer"
+                    onClick={handleChangeShowProfileMenu}
+                >
                     <img src={localUserData?.agent?.imageUrl} alt="" className='w-8 h-8 rounded-full border' />
                     <div className="flex gap-2 items-center">
                         <p>{localUserData?.agent?.prenom} {localUserData?.agent?.nom}</p>
@@ -40,6 +83,16 @@ const NavBar = () => {
                     />
                 </div>
             </div>
+            {
+                showMenu &&
+                <div className={`absolute top-12 right-0 bg-white border z-10 shadow-xl p-4 min-w-min w-80 popup ${hasTransitionedIn && 'isIn'} ${showMenu && 'isVisible'}`}>
+                    <div className="px-8">
+                        <div>
+                            {ProfileMenu()}
+                        </div>
+                    </div>
+                </div>
+            }
         </nav>
     );
 }
