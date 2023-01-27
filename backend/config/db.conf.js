1dea1c9178065eba7e9769972961c6bd15202527
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-01-27 10:11:33 +0100

    dbport .env file

--- backend/config/db.conf.js
+++ backend/config/db.conf.js
@@ -7,9 +7,10 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbpassword = process.env.DB_PWD;
 const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_HOST;
 
 export const dbSequelize = new Sequelize(dbName, dbUser, dbpassword, {
     dialect: 'mysql',
     host: dbHost,
-    port: 3306
+    port: dbPort
 });
