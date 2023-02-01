b084b972cd759f3cf1c1245c3a96d47bcd91def0
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-02-01 09:10:41 +0100

    fix: init fiche periodique par produit

--- frontend/src/admin/components/patrimoine/FicheStockPeriode.jsx
+++ frontend/src/admin/components/patrimoine/FicheStockPeriode.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactToPrint from 'react-to-print';
 
-import FicheStock from '../docs/FicheStock';
+// import FicheStock from '../docs/FicheStock';
 import { useStateContext } from '../../../context/ContextProvider';
 import { handleGet } from '../../../api/get';
 import { INVENTAIRE_BASE_URL } from '../../../utils/constants';
+import Entete from '../docs/Entete';
 
 const FicheStockPeriode = () => {
+
+    const ficheRef = useRef();
     const { localUserData } = useStateContext();
 
     const [ficheProd, setFicheProd] = useState();
@@ -26,13 +30,152 @@ const FicheStockPeriode = () => {
     const dataSortie = ficheProd?.data?.find(fiche => fiche.typeOp === 'sortie');
 
     return (
-        <div>
-            <FicheStock
-                periode={''}
-                dataEntree={dataEntree}
-                dataSortie={dataSortie}
-                dataStock={articlesList?.data}
-            />
+        <div className='overflow-scroll'>
+
+            <div ref={ficheRef}>
+                <Entete />
+                <h1 className='mb-5 text-center font-semibold text-slate-700'> Fiches des stocks</h1>
+                <div>
+
+                    <div className='flex'>
+                        <table className="text-sm text-gray-700 overflow-scroll border border-slate-400 w-full">
+                            <thead className="bg-slate-100 text-gray-800 text-xs uppercase font-medium">
+                                <tr>
+                                    <th colSpan={5} className='font-bold text-center border-b py-2'>Entrees</th>
+                                </tr>
+                                <tr>
+                                    <th>N°</th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        Date
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        Désignation
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-center tracking-wider">
+                                        libelle
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        qté
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-gray-100">
+                                {dataEntree?.data && JSON.parse(dataEntree?.data).map((data, index) => {
+                                    const formattedData = data;
+                                    return <tr className="bg-slate-500 even:bg-slate-50 bg-opacity-20" key={formattedData.designation}>
+                                        <td className="pl-1">
+                                            {index + 1}
+                                        </td>
+                                        <td className="pl-4">
+                                            {formattedData.date}
+                                        </td>
+                                        <td className="flex px-6 py-4 whitespace-nowrap capitalize">
+                                            {formattedData.designation}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-center">
+                                            {formattedData.libelle}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            {formattedData.quantite}
+                                        </td>
+                                    </tr>
+                                }
+                                )}
+                            </tbody>
+                        </table>
+                        <table className="text-sm text-gray-700 overflow-scroll border border-slate-400 w-full">
+                            <thead className="bg-slate-100 text-gray-800 text-xs uppercase font-medium">
+                                <tr>
+                                    <th colSpan={5} className='font-bold text-center border-b py-2'>Sorties</th>
+                                </tr>
+                                <tr>
+                                    <th>N°</th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        Date
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        Désignation
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-center tracking-wider">
+                                        libelle
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                        qté
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-gray-100">
+                                {dataSortie?.data && JSON.parse(dataSortie?.data).map((data, index) => {
+                                    const formattedData = data;
+                                    return <tr className="bg-slate-500 even:bg-slate-50 bg-opacity-20" key={formattedData.designation}>
+                                        <td className="pl-1">
+                                            {index + 1}
+                                        </td>
+                                        <td className="pl-4">
+                                            {formattedData.date}
+                                        </td>
+                                        <td className="flex px-6 py-4 whitespace-nowrap capitalize">
+                                            {formattedData.designation}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-center">
+                                            {formattedData.libelle}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            {formattedData.quantite}
+                                        </td>
+                                    </tr>
+                                }
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                    <table className="min-w-full text-sm text-slate-800">
+                        <thead className="bg-slate-100 text-gray-800 text-xs uppercase font-medium">
+                            <tr>
+                                <th colSpan={5} className='font-bold text-center border-b py-2'>Stocks</th>
+                            </tr>
+                            <tr>
+                                <th></th>
+                                <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                    Désignation
+                                </th>
+                                <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                    Qté
+                                </th>
+                                <th scope="col" className="px-6 py-3 text-left tracking-wider">
+                                    Unité
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="bg-gray-100">
+                            {articlesList?.data?.map(({ designation, quantite, unite, id }, index) =>
+                                <tr className="bg-slate-500 even:bg-slate-100 bg-opacity-20" key={index}>
+                                    <td className="pl-4">
+                                        {index + 1}
+                                    </td>
+                                    <td className="flex px-6 py-4 whitespace-nowrap capitalize">
+                                        {designation}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        {quantite}
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        {unite.libelle}
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+            <div className='mt-5'>
+                <ReactToPrint
+                    trigger={() => <button className='p-1 border-[0.2px] border-sky-500 text-sm text-sky-500 hover:text-red-600'>Imprimer</button>}
+                    content={() => ficheRef.current}
+                    pageStyle="@page {size: landscape}"
+                />
+            </div>
         </div>
     );
 }
