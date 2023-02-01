e4202bec715ec5f6019a9e2df04bd979097cbccc
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-02-01 09:42:11 +0100

    feat: front-fetch sepcific product fiche

--- frontend/src/admin/components/patrimoine/FicheStockPeriode.jsx
+++ frontend/src/admin/components/patrimoine/FicheStockPeriode.jsx
@@ -6,33 +6,71 @@ import { useStateContext } from '../../../context/ContextProvider';
 import { handleGet } from '../../../api/get';
 import { INVENTAIRE_BASE_URL } from '../../../utils/constants';
 import Entete from '../docs/Entete';
+import { handleChange } from '../../../utils/onChange';
+import Input from '../../../components/Input';
 
 const FicheStockPeriode = () => {
 
     const ficheRef = useRef();
     const { localUserData } = useStateContext();
 
+    const [choosedArtEntrie, setChoosedArtEntrie] = useState();
     const [ficheProd, setFicheProd] = useState();
     const [articlesList, setArticlesList] = useState();
 
     useEffect(() => {
+        setFicheProd({})
         handleGet(
             localUserData?.token,
-            `${INVENTAIRE_BASE_URL}/operation/prodperiode/f0429ce0-c17a-4e8a-8f29-2b2712ec2927?debut=2023-01-01&fin=2023-01-31`,
+            `${INVENTAIRE_BASE_URL}/operation/prodperiode/${choosedArtEntrie}?debut=2023-01-01&fin=2023-01-31`,
             setFicheProd,
             null
         );
+    }, [choosedArtEntrie]);
+    useEffect(() => {
         handleGet(localUserData?.token, `${INVENTAIRE_BASE_URL}/article/all`, setArticlesList, null);
     }, []);
+
 
     const dataEntree = ficheProd?.data?.find(fiche => fiche.typeOp === 'entree');
 
     const dataSortie = ficheProd?.data?.find(fiche => fiche.typeOp === 'sortie');
 
     return (
-        <div className='overflow-scroll'>
-
-            <div ref={ficheRef}>
+        <div className='mt-10'>
+            <div className='mb-5 text-lg font-semibold text-slate-600'>
+                Choisissez un produit et un intervall de date
+            </div>
+            <div className='flex gap-10 items-start text-slate-400'>
+                <div>
+                    <label for="" className='font-bold text-sm'>Article</label>
+                    <select
+                        value={choosedArtEntrie}
+                        onChange={(e) => handleChange(e, setChoosedArtEntrie)}
+                        className="text-gray-700 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-3 px-4 block mt-2"
+                    >
+                        <option value="" disabled hidden selected>Choisir article</option>
+                        {articlesList?.data?.map((option) =>
+                            <option
+                                key={option.id}
+                                value={`${option.id}`}
+                                className='capitalize'
+                            >
+                                {option.designation}
+                            </option>
+                        )}
+                    </select>
+                </div>
+                <Input
+                    label='Début'
+                    type='date'
+                />
+                <Input
+                    label='Fin'
+                    type='date'
+                />
+            </div>
+            {choosedArtEntrie && <div ref={ficheRef}>
                 <Entete />
                 <h1 className='mb-5 text-center font-semibold text-slate-700'> Fiches des stocks</h1>
                 <div>
@@ -129,7 +167,7 @@ const FicheStockPeriode = () => {
                             </tbody>
                         </table>
                     </div>
-                    <table className="min-w-full text-sm text-slate-800">
+                    {/* <table className="min-w-full text-sm text-slate-800">
                         <thead className="bg-slate-100 text-gray-800 text-xs uppercase font-medium">
                             <tr>
                                 <th colSpan={5} className='font-bold text-center border-b py-2'>Stocks</th>
@@ -165,10 +203,10 @@ const FicheStockPeriode = () => {
                                 </tr>
                             )}
                         </tbody>
-                    </table>
+                    </table> */}
                 </div>
 
-            </div>
+            </div>}
             <div className='mt-5'>
                 <ReactToPrint
                     trigger={() => <button className='p-1 border-[0.2px] border-sky-500 text-sm text-sky-500 hover:text-red-600'>Imprimer</button>}
